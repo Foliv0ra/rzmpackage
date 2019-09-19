6804c9f95c7a4bd54e314006d0678d613c428a52
Christian Rupp <rupp.dev@gmail.com>
2019-09-19 13:33:36 +0200

    updated dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,18 +4,20 @@ $EM_CONF[$_EXTKEY] = [
    'description' => 'The basic Site Skelleton',
    'category' => 'templates',
    'author' => 'Christian Rupp',
-   'author_email' => 'rupp@raumzeitmedia.de',
+   'author_email' => 'rupp.dev@gmail.com',
    'author_company' => 'Raumzeitmedia GmbH',
    'version' => '1.0.0',
    'state' => 'stable',
    'constraints' => [
       'depends' => [
          'typo3' => '8.7.0-9.5.99',
-         'fluid_styled_content' => '8.7.0-9.5.99',
-         'content_defender' => '3.0.11',
-	 'mask' => '4.1.2'
+         'fluid_styled_content' => '8.7.0-9.5.99'
       ],
       'conflicts' => [
+      ],
+      'suggests' => [
+         'content_defender' => '3.0.11-3.0.99',
+	      'mask' => '4.1.2-4.1.99'
       ],
    ],
    'uploadfolder' => 0,
