1c01f47cdc54ce1d0c4ed9e3d50fa9183a5bf323
Christian Rupp <rupp.dev@gmail.com>
2019-09-19 16:25:18 +0200

    Added ext_localconf to initiate PageTSConfig and RTE-Config

---
+++ ext_localconf.php
@@ -0,0 +1,13 @@
+<?php
+defined('TYPO3_MODE') || die();
+
+/***************
+ * Add default RTE configuration
+ */
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['rzm'] = 'EXT:' . $_EXTKEY . '/Configuration/RTE/Default-RTE-package.yaml';
+
+/***************
+ * PageTS
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="DIR:EXT:' . $_EXTKEY . '/Configuration/TsConfig/PageTSConfig/BackendLayouts/">');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="DIR:EXT:' . $_EXTKEY . '/Configuration/TsConfig/PageTSConfig/imageManipulation.tsconfig">');
